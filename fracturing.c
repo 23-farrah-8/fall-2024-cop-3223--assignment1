dee9f463a99c86ca412ac047a3b3813c3a245274
23-farrah-8 <farrahmccloud@gmail.com>
2024-09-08 21:03:34 +0000

    fracturing

--- fracturing.c
+++ fracturing.c
@@ -18,16 +18,18 @@
 // Defining PI
 #define PI 3.14159
 
-//Defining variables
+//Defining variables. I changed y1 into yVal1 because y1 was already in the math library.
 double x1, x2;
 double yVal1, yVal2;
 
+//This makes sure the distance doesn't repeat when area and perimeter are calculated.
 double noRepeat = 0;
 
 // Calculates distance between x and y points.
 double calculateDistance()
 {
     double distance = sqrt(pow(x2-x1, 2) + pow(yVal2-yVal1, 2));
+    // If statement so this only prints out once.
     if (noRepeat == 0) 
     {
         printf("\nThe distance between the two points is %.3f", distance);
@@ -36,8 +38,10 @@ double calculateDistance()
     return distance;
 }
 
+// Calculates perimeter for a circle.
 double calculatePerimeter()
 {
+    // I used double distance so I didn't have to repeat the math for distance over.
     double distance = calculateDistance();
     double perimeter = (2 * PI * distance);
     printf("\nThe perimeter of the city encompassed by your request is %.3f", perimeter);
@@ -45,6 +49,7 @@ double calculatePerimeter()
     return 4.0;
 }
 
+// Calculates the area for x and y points.
 double calculateArea ()
 {
     double distance = calculateDistance();
@@ -54,6 +59,7 @@ double calculateArea ()
     return 2.0;
 }
 
+// Calculates the width for the x values.
 double calculateWidth()
 {
     double width = (x2-x1);
@@ -62,6 +68,7 @@ double calculateWidth()
     return 1.0;
 }
 
+// Calculates the height for the y values.
 double calculateHeight()
 {
     double height = (yVal2-yVal1);
@@ -70,17 +77,21 @@ double calculateHeight()
     return 1.0;
 }
 
+// Main function with arguments.
 int main(int argc, char**argv)
 {
+    // Asks user for x1, x2, y1, y2 values.
     printf("\nEnter x1, x2:");
     scanf("%lf %lf", &x1, &x2);
     printf("\nEnter y1, y2:");
     scanf("%lf %lf", &yVal1, &yVal2);
 
+    // Prints the user's values out.
     printf("\nPoint #1 entered: x1 = %.3f, y1 = %.3f", x1, yVal1);
     printf("\nPoint #2 entered: x2 = %.3f, y2 = %.3f", x2, yVal2);
     
-
+    // This calls all the functions and uses the user's values to calculate distance, perimeter, area,
+    // width, and height.
     calculateDistance();
     calculatePerimeter();
     calculateArea();
